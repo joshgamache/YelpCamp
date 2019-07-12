322f1d6b1ba8762ec4e822010895d70099d571eb
Josh Gamache <jsgamache@gmail.com>
2019-07-12 13:34:11 -0600

    Refactor v1 (#3)
    * Add express-sanitizer and method-override
    
    * Cleaunup code
    
    * Added edit/update/distroy routes
    
    * Refactoring to clean up code
    
    * npm updates
    
    * Refactoring to clean up code
    
    * Updating ejs files
    
    * Updating ejs files

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const 	express = require("express"),
 		app = express(),
 		bodyParser = require("body-parser"),
-		mongoose = require("mongoose");
+		mongoose = require("mongoose"),
+		methodOverride = require("method-override"),
+		expressSanitizer = require("express-sanitizer");
 
 // Set up MongoDB/mongoose using ATLAS to make it server-independent (code pulled from MongoDB atlas page )
 const mongoURI = "mongodb+srv://devidle:" + process.env.MDBauth + "@cluster0-jcmtm.mongodb.net/test?retryWrites=true&w=majority";
@@ -17,10 +19,13 @@ mongoose.connect(mongoURI, {
 });
 
 // module activation and linking
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(expressSanitizer());
+
+// override with POST having ?_method=PUT or ?_method=DELETE
+app.use(methodOverride('_method'));
 
 // Schema and model
 var campgroundSchema = new mongoose.Schema({
@@ -37,7 +42,7 @@ app.get("/", (req, res) => {
 
 
 // INDEX Campgrounds page
-app.get("/campgrounds", (req, res) => {
+app.get("/campground", (req, res) => {
 	Campground.find({}, (err, allCampgrounds) => {
 		if (err) {
 			console.log(err);
@@ -50,44 +55,68 @@ app.get("/campgrounds", (req, res) => {
 });
 
 // NEW New campground page
-app.get("/campgrounds/new", (req, res) => {
+app.get("/campground/new", (req, res) => {
 	res.render("new");
 });
 
 
 // CREATE Add to campgrounds page
-app.post("/campgrounds", (req, res) => {
-	// get data from form and add to the campgrounds array
-	var newCamp = {
-		name: req.body.campName,
-		image: req.body.campImg,
-		description: req.body.campDescription
-	};
-	// Take newly created campground from form and make a new entry
-	Campground.create(newCamp, (err, newlyCreated) => {
+app.post("/campground", (req, res) => {
+	req.body.campground.body = req.sanitize(req.body.campground.body);
+	Campground.create(req.body.campground, (err, newlyCreatedCampground) => {
 		if (err) {
 			console.log(err);
 		} else {
-			// res.render("campgrounds", {campgrounds:allCampgrounds});
-			res.redirect("/campgrounds");
-		}
-		// redirect back to campgrounds page
-
+			console.log(newlyCreatedCampground)
+			res.redirect("/campground");
+		}	// redirect back to campgrounds index page
 	});
-
-	// campgrounds.push(newCamp);
-	// res.send("You have reached the post route!");
 });
 
 // SHOW shows more info about a single campground
-app.get("/campgrounds/:id", (req, res) => {
+app.get("/campground/:id", (req, res) => {
 	Campground.findById(req.params.id, (err, foundCampground) => {
 		if (err) {
 			console.log(err);
 		} else {
 			res.render("show", {
-				campDetails: foundCampground
+				campground: foundCampground
 			});
+		}
+	});
+});
+
+// EDIT
+app.get("/campground/:id/edit", (req, res) => {
+	Campground.findById(req.params.id, (err, foundCampground) =>{
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("edit", {campground: foundCampground});
+		}
+	});
+});
+
+// UPDATE
+app.put("/campground/:id", (req, res) => {
+	req.body.campground.body = req.sanitize(req.body.campground.body);
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updateCampground) => {
+		if (err) {
+			console.log(error);
+		} else {
+			console.log(updateCampground);
+			res.redirect("/campground/" + req.params.id);
+		}
+	});
+});
+
+// DESTROY
+app.delete("/campground/:id", (req, res) => {
+	Campground.findByIdAndDelete(req.params.id, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/campground");
 		}
 	});
 });
