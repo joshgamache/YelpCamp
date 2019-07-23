a46d38303c87067ad34913a2146ae6954610568b
Josh Gamache <jsgamache@gmail.com>
2019-07-23 20:11:38 +0000

    started edit routes on comments

--- routes/comments.js
+++ routes/comments.js
@@ -13,6 +13,7 @@ const isLoggedIn = (req, res, next) => {
     res.redirect("/login");
 };
 
+
 /*
 Comment routes
 */
@@ -52,5 +53,38 @@ router.post("/", isLoggedIn, (req, res) => {
         }
     });
 });
+
+// EDIT
+router.get("/:comment_id/edit", (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {comment: foundComment, campground_id: req.params.id});
+        }
+    });
+});
+
+// UPDATE
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+});
+
+// DESTROY
+router.delete("/:comment_id", (req, res) => {
+    Comment.findByIdAndDelete(req.params.comment_id, (err) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+})
 
 module.exports = router;
